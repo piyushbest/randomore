21aae5c4e46392fa6a8f6f698b932066bd212b47
piyushbest <42518831+piyushbest@users.noreply.github.com>
2018-12-23 11:18:01 +0530

    Add files via upload

---
+++ src/RaindomOre/Generate.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace RandomOre;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\math\Vector3;
+use pocketmine\Player;
+use pocketmine\event\block\BlockUpdateEvent;
+use pocketmine\item\Item;
+use pocketmine\event\Listener;
+use pocketmine\level\Level;
+use pocketmine\block\Block;
+use pocketmine\block\IronOre;
+use pocketmine\block\Cobblestone;
+use pocketmine\block\DiamondOre;
+use pocketmine\block\EmeraldOre;
+use pocketmine\block\GoldOre;
+use pocketmine\block\CoalOre;
+use pocketmine\block\lava;
+use pocketmine\block\LapisOre;
+use pocketmine\block\RedstoneOre;
+use pocketmine\block\Water;
+
+class Generate extends PluginBase implements Listener{
+    
+    public function onEnable(){
+        $this->getLogger()->info("Plugin RandomOre by piyushbest!");
+        $this->getServer()->getPluginManager()->registerEvents($this,$this);
+    }
+
+        public function onBlockSet(BlockUpdateEvent $event){
+        $block = $event->getBlock();
+        $water = false;
+        $lava = false;
+        for ($i = 2; $i <= 5; $i++) {
+            $nearBlock = $block->getSide($i);
+            if ($nearBlock instanceof Water) {
+                $water = true;
+            } else if ($nearBlock instanceof lava) {
+                $lava = true;
+            }
+            if ($water && $lava) {
+                $id = mt_rand(1, 20);
+                switch ($id) {
+                    case 2;
+                        $newBlock = new IronOre();
+                        break;
+                    case 4;
+                        $newBlock = new GoldOre();
+                        break;
+                    case 6;
+                        $newBlock = new EmeraldOre();
+                        break;
+                    case 8;
+                        $newBlock = new CoalOre();
+                        break;
+                    case 10;
+                        $newBlock = new RedstoneOre();
+                        break;
+                    case 12;
+                        $newBlock = new DiamondOre();
+                        break;
+					case 14;
+                        $newBlock = new LapisOre();
+                        break;	
+                    default:
+                        $newBlock = new Cobblestone();
+                }
+                $block->getLevel()->setBlock($block, $newBlock, true, false);
+                return;
+            }
+        }
+    }
+}
